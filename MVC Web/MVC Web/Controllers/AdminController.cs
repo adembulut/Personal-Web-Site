b151d1f49b8c7332d0f2e189143bad98869de89e
adembulut <adembulutapp@gmail.com>
2017-10-13 16:41:13 +0300

    cif

--- MVC Web/MVC Web/Controllers/AdminController.cs
+++ MVC Web/MVC Web/Controllers/AdminController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MVC_Web.Models;
+using System.Web.Helpers;
+using System.IO;
 namespace MVC_Web.Controllers
 {
     public class AdminController : BaseController
@@ -42,10 +44,54 @@ namespace MVC_Web.Controllers
                 db.SaveChanges();
                 ViewBag.result = "Save is completed.";
                 return View(eski);
-                
+
             }
             ViewBag.result = "An error occurred.";
             return View(eski);
+        }
+
+        public ActionResult BlogAdd()
+        {
+            return View(db.Categories.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult BlogAdd(Article art, HttpPostedFileBase foto)
+        {
+            try
+            {
+                string path = Server.MapPath("~/Content/uploads/");
+                Directory.CreateDirectory(path);
+                WebImage img = new WebImage(foto.InputStream);
+                img.Resize(600, 480, false, false);
+                FileInfo fotoinfo = new FileInfo(foto.FileName);
+                string newfoto = Guid.NewGuid().ToString() + fotoinfo.Extension;
+                img.Save("~/Content/uploads/" + newfoto);
+                string imagePath = "~/Content/uploads/" + newfoto;
+                Article article = new Article
+                {
+                    CategoryID = art.CategoryID,
+                    Content = art.Content,
+                    Header = art.Header,
+                    ImagePath = imagePath,
+                    AddedDate = DateTime.Now,
+                    UserID = (Session["Admin"] as User).Id
+                };
+                db.Articles.Add(article);
+                db.SaveChanges();
+
+                return RedirectToAction("Blogs");
+            }
+            catch (Exception)
+            {
+                return View(db.Categories.ToList());
+            }
+
+        }
+
+        public ActionResult Blogs()
+        {
+            return View(db.Articles.ToList());
         }
     }
 }
